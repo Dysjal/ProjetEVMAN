2bfc1ff1466696af292a759ae60e8ea9eb7db249
Dysjal <quentincheron3468@gmail.com>
2019-11-18 16:11:15 +0100

    avancement du code

--- click.js
+++ click.js
@@ -1,9 +1,12 @@
 var click = 0 ;
 var nbDeJeu = 0;
+var seconds_left ;
+
 function incrementer(){
  click++;
  document.getElementById("insert").innerHTML = click;
 }
+
 function Timer() {
 var x = document.getElementById("Jeu");
  x.style.display = "none";
@@ -16,10 +19,35 @@ var x = document.getElementById("Jeu");
     }
  document.getElementById("ancien").innerHTML = click;
 }
+
 function Lancer() {
+  seconds_left = 3;
   click = -1
+
+  document.getElementById('timer_div').innerHTML = seconds_left;
+
   var x = document.getElementById("Jeu");
     x.style.display = "block";
+    
   var y = document.getElementById("affichage");
     y.style.display = "none";
+
+   var interval = setInterval(function() {
+    document.getElementById('timer_div').innerHTML = --seconds_left;
+
+    if (seconds_left <= 0)
+    {
+        clearInterval(interval);
+    }
+}, 1000);
 }
+/*
+  function tempsRestant(){
+    document.getElementById('timer_div').innerHTML = --seconds_left;
+  }
+
+while (seconds_left !== 0){
+      setInterval(tempsRestant(),1000);
+          console.log(seconds_left);
+    }
+*/
