106ef0fd0216dddc0ac2b9751387de5ad88675b8
Dysjal <quentincheron3468@gmail.com>
2019-11-22 18:05:42 +0100

    Ajout final boxe

--- Boxe/Boxe.js
+++ Boxe/Boxe.js
@@ -1,34 +1,73 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-var click = 0 ;
-var nbDeJeu = 0;
+var touche = 0;
 var seconds_left ;
+var nbDeJeu = 0;
 
 function Lancer() {
-  seconds_left = 3;
-  click = -1
+  touche = 0;
+  document.getElementById('touche').innerHTML = touche;
+ 
+  var y = document.getElementById("Afficher");
+    y.style.display = "block";
+     var c = document.getElementById("rond");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(38,38,36,0,2*Math.PI);
+ctx.stroke();
+  var t = Math.floor((Math.random() * 31) + 0);
+  var l = Math.floor((Math.random() * 95) + 0);
+  c.style.marginTop =t+"%";
+  c.style.marginLeft = l+"%";
+}
 
-  document.getElementById('timer_div').innerHTML = seconds_left;
+function Timer(){
+    seconds_left = 15;
+    document.getElementById('timer_div').innerHTML = seconds_left;
 
   var x = document.getElementById("Jeu");
     x.style.display = "block";
     
-  var y = document.getElementById("affichage");
+  var y = document.getElementById("menu");
     y.style.display = "none";
+    if(nbDeJeu === 0){
+      var z = document.getElementById("nbDeJeu");
+      z.style.display = "block";
+      nbDeJeu ++;
+    }
+
 
    var interval = setInterval(function() {
     document.getElementById('timer_div').innerHTML = --seconds_left;
-
+    console.log(seconds_left);
     if (seconds_left <= 0)
     {
+    var y = document.getElementById("Afficher");
+    y.style.display = "none";
+     var x = document.getElementById("Jeu");
+    x.style.display = "none";
+    var z = document.getElementById("menu");
+    z.style.display = "block";
+    document.getElementById("ancien").innerHTML = touche;
         clearInterval(interval);
     }
 }, 1000);
+
 }
 function Creation (){
-
-  ctx.beginPath();
-ctx.arc(95,50,40,0,2*Math.PI);
-ctx.stroke();
+    var y = document.getElementById("Afficher");
+    y.style.display = "none";
+     var x = document.getElementById("Jeu");
+    x.style.display = "none";
+    var z = document.getElementById("nbDeJeu");
+    z.style.display = "none";
+ 
 }
+function Clicked (){
+  touche++;
+  document.getElementById('touche').innerHTML = touche;
+  var c = document.getElementById("rond");
+  var t = Math.floor((Math.random() * 31) + 0);
+  var l = Math.floor((Math.random() * 95) + 0);
+  c.style.marginTop =t+"%";
+  c.style.marginLeft = l+"%";
+  }
 
