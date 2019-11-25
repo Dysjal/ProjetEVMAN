950d4a1d56b8b3e740991bbbb8580388e0815798
Bagalrel <bagalrel@gmail.com>
2019-11-25 02:06:19 +0100

    celinee

--- Natation/Natation.js
+++ Natation/Natation.js
@@ -2,7 +2,6 @@ var score = 0;
 var seconds_left ;
 var nbDeJeu = 0;
 var bool=0;
-var classement = [42,41,37,34,29,25,22,22,20,19];
 
 function Lancer() {
  
@@ -38,8 +37,6 @@ function Timer(){
     var z = document.getElementById("menu");
     z.style.display = "block";
     document.getElementById("ancien").innerHTML = score;
-            classe(score);
-
         clearInterval(interval);
     }
 }, 1000);
@@ -52,8 +49,6 @@ function Creation (){
     x.style.display = "none";
     var z = document.getElementById("nbDeJeu");
     z.style.display = "none";
-            classe(score);
-
 }
 
 function clickGauche(){
@@ -70,26 +65,3 @@ function clickDroit(){
     bool=0;
   }
 }
- function classe (point){
-    for ( var i =0; i<10; i++){
-      if(point >= classement[i]){
-        classement[i]=point;
-        i=10;
-
-      }
-     }
-     document.getElementById("class1").innerHTML ="1 : "+ classement[0];
-     document.getElementById("class2").innerHTML ="2 : "+ classement[1];
-     document.getElementById("class3").innerHTML ="3 : "+ classement[2];
-     document.getElementById("class4").innerHTML ="4 : "+ classement[3];
-     document.getElementById("class5").innerHTML ="5 : "+ classement[4];
-     document.getElementById("class6").innerHTML ="6 : "+ classement[5];     
-     document.getElementById("class7").innerHTML ="7 : "+ classement[6];
-     document.getElementById("class8").innerHTML ="8 : "+ classement[7];
-     document.getElementById("class9").innerHTML ="9 : "+ classement[8];
-    document.getElementById("class10").innerHTML ="10 : "+ classement[9];
-
-
-
-   
-  }
